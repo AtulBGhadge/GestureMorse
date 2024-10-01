fda5478246dcbfca4a08581aa75635a2f785d959
AtulBGhadge <125268092+AtulBGhadge@users.noreply.github.com>
2024-10-01 11:56:34 +0530

    Update NRF24L01_Receiver.ino

--- Arduino/NRF24L01_Receiver.ino
+++ Arduino/NRF24L01_Receiver.ino
@@ -1,18 +1,23 @@
 #include <SPI.h>
 #include <RF24.h>
 
-RF24 radio(9, 10); // CE and CSN pins for NRF24L01
+RF24 radio(9, 10); // NRF24L01 CE and CSN pins
+
+const byte address[6] = "00001";
 
 void setup() {
   Serial.begin(9600);
   radio.begin();
-  radio.startListening(); // Simplified setup
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
 }
 
 void loop() {
   if (radio.available()) {
-    char buffer[32]; // Buffer for incoming data
-    radio.read(&buffer, sizeof(buffer));
-    Serial.println(buffer); // Print the received data
-  }
+    char receivedMessage[32]; // Buffer to store received data
+    radio.read(&receivedMessage, sizeof(receivedMessage));
+    Serial.print("Message: ");
+    Serial.println(receivedMessage); // Print the received message
+  }
 }
